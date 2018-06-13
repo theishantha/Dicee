6ed15bd0eb2b34e1d6dc1b9bf6a2b804dbc84ac2
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-14 02:45:19 +0530

    starting coding

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
